75d4771a338aef4af0a5682f66902b799f780e88
rubengr16 <55591651+rubengr16@users.noreply.github.com>
2020-12-27 17:18:47 +0100

    changes in standard

--- 2_using_print/1_hello_world.py
+++ 2_using_print/1_hello_world.py
@@ -1,5 +1,5 @@
 # hello world program
 
 # print function displays messages
-print("hello world")  # double quotes will be used as standard
-print('hello world')  # single quotes also possible
+print('hello world')  # single quotes will be used as standard
+print("hello world")  # double quotes also possible
