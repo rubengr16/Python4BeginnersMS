ed4b5aa12b86829a26d4a8d70ae8493eede0f9c6
rubengr16 <ruben16gallego@gmail.com>
2020-12-28 02:04:09 +0100

    minor changes

--- 2_using_print/1_hello_world.py
+++ 2_using_print/1_hello_world.py
@@ -1,5 +1,5 @@
 # hello world program
 
 # print function displays messages
-print("hello world")  # double quotes will be used as standard
+print("hello world")  # double quotes will be used as convention
 print('hello world')  # single quotes also possible
--- 2_using_print/2_ask_for_input.py
+++ 2_using_print/2_ask_for_input.py
@@ -1,5 +1,5 @@
 # input function
 name = input("Please enter your name: ")  # input whit message
-print("Enter your age")
+print("Enter your age: ")
 age = input()  # simple input
 print(name, "your age is", age)
